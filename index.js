17c3e15138fb675e548aa1e1fcf8a67abfc93009
Carlos Marcelo Torres Vargas <carlosmarcelotorresvargas@gmail.com>
2022-02-28 15:07:34 -0400

    WebServer Listo

---
+++ index.js
@@ -0,0 +1,32 @@
+const express = require('express');
+const app = express();
+const hbs = require('hbs');
+require('dotenv').config();
+
+
+// handlebars
+app.set('view engine', 'hbs');
+hbs.registerPartials(__dirname + '/views/partials');
+
+// servir contenido estatico
+app.use(express.static('public'))
+
+
+app.use('/', (req, res)=>{
+    res.render('home')
+})
+app.use('/generic', (req, res)=>{
+    res.render('generic')
+})
+app.use('/elements', (req, res)=>{
+    res.render('elements')
+})
+app.use('*', (req, res)=>{
+    res.render('404')
+})
+
+
+
+
+app.listen(process.env.PORT)
+
